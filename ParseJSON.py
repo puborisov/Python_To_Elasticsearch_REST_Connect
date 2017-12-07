df0f0a4ecea27ebe6ff4eac47cc86f1b55ca652a
Pavel Borisov <puborisov@users.noreply.github.com>
2017-12-07 09:15:30 +0300

    Logging and cleaning
    Added:
    * logging
    * JSON clean up after parse

--- ParseJSON.py
+++ ParseJSON.py
@@ -1,77 +1,95 @@
 import json
 import pandas as pd
 import csv
+import os
+import re
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+import logging
+
+# Set up some logging
+logger = logging.getLogger('myapp')
+hdlr = logging.FileHandler('./logs/ParseJSON-{:%Y.%m.%d}.log'.format(datetime.now()))
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr) 
+logger.setLevel(logging.DEBUG)
 
 with open('./config/parse_config.csv') as csvfile:
     parse_config = csv.reader(csvfile, delimiter=',')
     for config in parse_config:
 
-        # Load data from json file
-        if str(config[2]).upper() == 'D':
-            date = datetime.now() + relativedelta(days=-int(config[1]))
-            file_path = '{}{}-{:%Y.%m.%d}.json'.format(config[3], config[0], date)
-        else:
-            date = datetime.now() + relativedelta(months=-int(config[1]))
-            file_path = '{}{}-{:%Y.%m}.json'.format(config[3], config[0], date)
-
-        with open(file_path, encoding='utf8') as file:
-            data_load = json.load(file)
-
-
-        # Parse JSON scroll records
-        data = []
-        for i in range(len(data_load)):
-            data += data_load[i]['hits']['hits']
-
-        # Init dic with dataframes
-        df = {}
-
-        # Create main dataframe
-        df['main'] = pd.io.json.json_normalize(data)
-
-        # Iterate over all DataFrames
-        d = 0
-        while d < len(df.keys()):
-
-            # Move list to "out" dict and mark their positions
-            out = {}
-            cur_df = list(df.keys())[d]
-
-            for col in df[cur_df].columns:
-                if col != '__value__'  and any(isinstance(n, list) for n in df[cur_df][col]):
-                    out[col] = df[cur_df][col]
-                    df[cur_df].drop(col, axis=1, inplace=True)
-
-
-            # Parse "out" data
-
-            for key in out.keys():
-                df_to_add = []
-                dict_flag = True
-                for list_num in range(len(out[key])):
-                    if isinstance(out[key][list_num], list) and (len(out[key][list_num])>0):
-                        if any(isinstance(n, dict) for n in out[key][list_num]):
-                            for list_item in out[key][list_num]:
-                                list_item.update({"_id": str(df['main'].iloc[int(list_num)]['_id'])})
-                            df_to_add += out[key][list_num]
-                        else:
-                            dict_flag = False
-                            df_to_add += list(map(list,
-                                                  zip(*[out[key][list_num],[df['main'].iloc[int(list_num)]['_id']]
-                                                        * len(out[key][list_num])])))
-
-                if dict_flag:
-                    df['{}.{}'.format(cur_df, key)] = pd.DataFrame(df_to_add)
-                else:
-                    df['{}.{}'.format(cur_df, key)] = pd.DataFrame(df_to_add, columns=['__value__','_id'])
-            d += 1
-
-        # Save to files
-        for key, value in df.items():
-            if str(config[2]).upper() == 'D':
-                file_path = '{}{}_{:%Y.%m.%d}.csv'.format(config[4], key, date)
+        for filename in os.listdir(config[1]):
+            file_name = str(filename)
+            
+            logger.info(config[1]+file_name)
+            
+            
+            # Get file date
+            if (re.search(r'\d{4}.\d{2}.\d{2}', file_name) is None):
+                match = re.search(r'\d{4}.\d{2}', file_name)
             else:
-                file_path = '{}{}_{:%Y.%m}.csv'.format(config[4], key, date)
-            value.to_csv(file_path, encoding='utf-8', index=False)
+                match = re.search(r'\d{4}.\d{2}.\d{2}', file_name)
+                
+            file_date = str(match.group())
+            
+            # Load data from json file              
+            with open(config[1]+file_name, encoding='utf8') as file:
+                data_load = json.load(file)
+
+            # Parse JSON scroll records
+            data = []
+            for i in range(len(data_load)):
+                data += data_load[i]['hits']['hits']
+
+            # Init dic with dataframes
+            df = {}
+
+            # Create main dataframe
+            df['main'] = pd.io.json.json_normalize(data)
+
+            # Iterate over all DataFrames
+            d = 0
+            while d < len(df.keys()):
+
+                # Move list to "out" dict and mark their positions
+                out = {}
+                cur_df = list(df.keys())[d]
+
+                for col in df[cur_df].columns:
+                    if col != '__value__'  and any(isinstance(n, list) for n in df[cur_df][col]):
+                        out[col] = df[cur_df][col]
+                        df[cur_df].drop(col, axis=1, inplace=True)
+
+
+                # Parse "out" data
+
+                for key in out.keys():
+                    df_to_add = []
+                    dict_flag = True
+                    for list_num in range(len(out[key])):
+                        if isinstance(out[key][list_num], list) and (len(out[key][list_num])>0):
+                            if any(isinstance(n, dict) for n in out[key][list_num]):
+                                for list_item in out[key][list_num]:
+                                    list_item.update({"_id": str(df['main'].iloc[int(list_num)]['_id'])})
+                                df_to_add += out[key][list_num]
+                            else:
+                                dict_flag = False
+                                df_to_add += list(map(list,
+                                                      zip(*[out[key][list_num],[df['main'].iloc[int(list_num)]['_id']]
+                                                            * len(out[key][list_num])])))
+
+                    if dict_flag:
+                        df['{}.{}'.format(cur_df, key)] = pd.DataFrame(df_to_add)
+                    else:
+                        df['{}.{}'.format(cur_df, key)] = pd.DataFrame(df_to_add, columns=['__value__','_id'])
+                d += 1
+
+            # Save to files
+            for key, value in df.items():
+                file_path = '{}{}_{}.csv'.format(config[2], key, file_date)
+                value.to_csv(file_path, encoding='utf-8', index=False)
+            
+            # Delete source file
+            os.remove(config[1]+file_name)
+            logger.info('=> JSON parsed')
--- RestConnect.py
+++ RestConnect.py
@@ -3,12 +3,23 @@ import csv
 from datetime import datetime
 from elasticsearch2 import Elasticsearch
 from dateutil.relativedelta import relativedelta
+import logging
 
+# Set up some logging
+logger = logging.getLogger('myapp')
+hdlr = logging.FileHandler('./logs/RestConnect-{:%Y.%m.%d}.log'.format(datetime.now()))
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr) 
+logger.setLevel(logging.DEBUG)
+
+# Set connection
 try:
-    es = Elasticsearch(hosts='url', http_auth=('login', 'pass'), port=9200, timeout = 600)
-    print('Connected', es.info())
+    es = Elasticsearch(hosts='host', http_auth=('login', 'pass'), port=9200, timeout = 600)
+    logger.info('Connected: ', es.info())
 except Exception as ex:
-    print('Error:', ex)
+    logger.error(ex)
+
 
 def get_index(index, period, delta):
     if str(period).upper() == 'D':
@@ -18,42 +29,55 @@ def get_index(index, period, delta):
         date = datetime.now() + relativedelta(months=-int(delta))
         return '{}-{:%Y.%m}'.format(index, date)
 
-
+# Cycle through all indexes
 with open('./config/load_config.csv') as csvfile:
+    
     index_file = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in index_file:
-        index = get_index(row[0],row[2],row[1])
-        print(index)
+    indexes = []
+
+    for index_params in index_file:
+        indexes.append([index_params[0],index_params[1],index_params[2],index_params[3]])
+        if (str(index_params[2]).upper() != 'D') and (datetime.now().day==1):
+            indexes.append([index_params[0],'1','m',index_params[3]])
+            
+    for index_params in indexes:
         
-        # Initialize the request
-        query_body = '{"query": {"match_all": {}}}'
-        scroll = '1m'
-        timeout = 6000
-        size = 100
+        try:
+            index = get_index(index_params[0],index_params[2],index_params[1])
+            logger.info(index)
 
-        page = es.search(index=index, scroll=scroll, size=size, body=query_body, request_timeout=timeout)
+            # Initialize the request
+            query_body = '{"query": {"match_all": {}}}'
+            scroll = '1m'
+            timeout = 6000
+            size = 100
+            
+            page = es.search(index=index, scroll=scroll, size=size, body=query_body, request_timeout=timeout)
 
-        # Init scroll
-        sid = page['_scroll_id']
-        scroll_size = page['hits']['total']
-
-        # Init data
-        data = []
-        data.append(page)
-
-        # Start scrolling
-        while (scroll_size > 0):
-            page = es.scroll(scroll_id=sid, scroll=scroll)
-
-            # Update the scroll ID
+            # Init scroll
             sid = page['_scroll_id']
+            scroll_size = page['hits']['total']
 
-            # Get the number of results that we returned in the last scroll
-            scroll_size = len(page['hits']['hits'])
-
-            # Add to data list
+            # Init data
+            data = []
             data.append(page)
 
-        # Save to file
-        with open(row[3] + str(index) + '.json', 'w') as f:
-            json.dump(data, f)
+            # Start scrolling
+            while (scroll_size > 0):
+                page = es.scroll(scroll_id=sid, scroll=scroll)
+
+                # Update the scroll ID
+                sid = page['_scroll_id']
+
+                # Get the number of results that we returned in the last scroll
+                scroll_size = len(page['hits']['hits'])
+
+                # Add to data list
+                data.append(page)
+
+            # Save to file
+            with open(index_params[3] + str(index) + '.json', 'w') as f:
+                json.dump(data, f)
+            logger.info('=> Loaded successfully')
+        except Exception as ex:
+            logger.error(ex)
